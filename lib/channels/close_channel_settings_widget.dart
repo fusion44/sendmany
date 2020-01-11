ca475b99a99909ddfad2bf7986ce121f3af27b3e
Stefan Stammberger <some.fusion@gmail.com>
2020-01-11 10:51:30 +0100

    feat: add a close channel page

---
+++ lib/channels/close_channel_settings_widget.dart
@@ -0,0 +1,255 @@
+import 'package:fixnum/fixnum.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
+
+import '../common/constants.dart';
+import '../common/models/models.dart';
+import '../common/utils.dart';
+import '../common/widgets/widgets.dart';
+
+class CloseChannelSettingsWidget extends StatefulWidget {
+  final EstablishedChannel channel;
+  final Function(
+    bool force,
+    int blockTarget,
+    Int64 satPerByte,
+    String deliveryAddress,
+  ) onCloseChannelClick;
+
+  @override
+  _CloseChannelSettingsWidgetState createState() =>
+      _CloseChannelSettingsWidgetState();
+
+  CloseChannelSettingsWidget(this.channel, this.onCloseChannelClick);
+}
+
+class _CloseChannelSettingsWidgetState
+    extends State<CloseChannelSettingsWidget> {
+  bool _force = false;
+  int _blockTarget = 6;
+  Int64 _satPerByte;
+  final TextEditingController _deliveryAddressController =
+      TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 10,
+                  child: Text(
+                    widget.channel.channelPoint.fundingTxid,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                SizedBox(width: 2.0),
+                Expanded(
+                  flex: 1,
+                  child: Text(
+                    ':${widget.channel.channelPoint.outputIndex.toString()}',
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 4.0),
+            TranslatedText(
+              'channels.channel_point',
+              style: theme.textTheme.caption,
+            ),
+            SizedBox(height: 16.0),
+            OnchainFeeSelectWidget(
+              amountChanged: (OnchainFeeType type, Int64 amount, bool valid) {
+                if (type == OnchainFeeType.manual) {
+                  _satPerByte = amount;
+                  _blockTarget = null;
+                } else {
+                  _blockTarget = amount.toInt();
+                  _satPerByte = null;
+                }
+              },
+            ),
+            SizedBox(height: 4.0),
+            TranslatedText(
+              'channels.onchain_fee_types',
+              style: theme.textTheme.caption,
+            ),
+            SizedBox(height: 16.0),
+            Row(children: <Widget>[
+              Expanded(
+                child: FilledTextFieldManual(
+                  controller: _deliveryAddressController,
+                  textHint: tr(context, 'channels.optional_delivery_address'),
+                ),
+              ),
+              SizedBox(height: 4.0),
+              IconButton(
+                icon: Icon(Icons.content_paste),
+                onPressed: () async {
+                  var data = await Clipboard.getData('text/plain');
+                  _deliveryAddressController.text = data.text;
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.camera_alt),
+                onPressed: () => _scanBtcAddress(),
+              ),
+            ]),
+            SizedBox(height: 4.0),
+            TranslatedText(
+              'channels.optional_delivery_address',
+              style: theme.textTheme.caption,
+            ),
+            SizedBox(height: 16.0),
+            InkWell(
+              onTap: () => setState(() => _force = !_force),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TranslatedText(
+                      'channels.force_close',
+                      style: theme.textTheme.subhead,
+                    ),
+                  ),
+                  Checkbox(
+                    value: _force,
+                    onChanged: (bool value) {
+                      setState(() {
+                        _force = value;
+                      });
+                    },
+                  )
+                ],
+              ),
+            ),
+            widget.channel.active
+                ? TranslatedText(
+                    'channels.force_close_not_necessary_peer_online',
+                    style: theme.textTheme.caption.copyWith(
+                      color: sendManyPrimaryGreen500,
+                    ),
+                  )
+                : TranslatedText(
+                    'channels.force_close_necessary_peer_not_online',
+                    style: theme.textTheme.caption.copyWith(
+                      color: sendManyOrange300,
+                    ),
+                  ),
+            SizedBox(height: 32.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                    color: sendManyOrange300,
+                    child: TranslatedText('channels.close.close'),
+                    onPressed: () => _showAlert()),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _scanBtcAddress() async {
+    // onStringFound callback might be called multiple times
+    // make sure we only pop once
+    var popScheduled = false;
+    var result = await Navigator.push<String>(
+      context,
+      MaterialPageRoute(builder: (c) {
+        return QRScannerWidget(onStringFound: (String code) {
+          SchedulerBinding.instance.addPostFrameCallback((_) {
+            if (!popScheduled) {
+              popScheduled = true;
+              Navigator.of(c).pop(code);
+            }
+          });
+        });
+      }),
+    );
+
+    var qrInfo = checkQrCode(result);
+
+    if (qrInfo == null || qrInfo.layer != PaymentLayer.onchain) {
+      showSnackbar(
+        context,
+        'Found no valid onchain address:\n${qrInfo.address}',
+      );
+    } else {
+      _deliveryAddressController.text = qrInfo.address;
+    }
+  }
+
+  void _showAlert() async {
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      var result = await showDialog<bool>(
+        context: context,
+        builder: (BuildContext c) {
+          return AlertDialog(
+            title: TranslatedText('alert_dialog.confirm_action_title'),
+            content: _buildAlertContentWidget(),
+            actions: <Widget>[
+              FlatButton(
+                child: TranslatedText('alert_dialog.cancel'),
+                onPressed: () => Navigator.pop(context, false),
+              ),
+              FlatButton(
+                child: TranslatedText('alert_dialog.confirm'),
+                onPressed: () => Navigator.of(context).pop(true),
+              ),
+            ],
+          );
+        },
+      );
+
+      if (result != null && result is bool && result) {
+        widget.onCloseChannelClick(
+          _force,
+          _blockTarget,
+          _satPerByte,
+          _deliveryAddressController.text,
+        );
+      }
+    });
+  }
+
+  Widget _buildAlertContentWidget() {
+    var theme = Theme.of(context);
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        TranslatedText('channels.close.alert_query_really_close_channel'),
+        SizedBox(height: 16.0),
+        MoneyValueView(
+          amount: widget.channel.localBalance,
+          textAlign: TextAlign.left,
+        ),
+        TranslatedText(
+          'channels.close.you_receive',
+          textAlign: TextAlign.left,
+          style: theme.textTheme.caption,
+        ),
+        SizedBox(height: 16.0),
+        MoneyValueView(
+          amount: widget.channel.remoteBalance,
+          textAlign: TextAlign.left,
+        ),
+        TranslatedText(
+          'channels.close.partner_receives',
+          textAlign: TextAlign.left,
+          style: theme.textTheme.caption,
+        ),
+      ],
+    );
+  }
+}
--- lib/channels/list_channels_page.dart
+++ lib/channels/list_channels_page.dart
@@ -6,12 +6,18 @@ import 'package:material_design_icons_flutter/material_design_icons_flutter.dart
 import 'package:sendmany/channels/create_channel_page.dart';
 import 'package:sendmany/common/constants.dart';
 import 'package:sendmany/common/models/models.dart';
+import 'package:sendmany/common/utils.dart';
 import 'package:sendmany/common/widgets/charts/charts.dart';
 import 'package:sendmany/common/widgets/widgets.dart';
 import 'package:sendmany/wallet/balance/bloc/bloc.dart';
 
+import '../common/constants.dart';
+import '../common/models/models.dart';
+import '../common/widgets/widgets.dart';
+import 'close_channel_page.dart';
 import 'display_channel_info_page.dart';
 import 'subscribe_channel_events/bloc/bloc.dart';
+import 'close_channel/bloc.dart';
 
 class ListChannelsPage extends StatefulWidget {
   @override
@@ -38,54 +44,74 @@ class ListChannelsPage extends StatefulWidget {
 class _ListChannelsPageState extends State<ListChannelsPage> {
   int _blockHeight = 0;
   StreamSubscription _sub;
+  CloseChannelBloc _closeChannelBloc;
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder(
-      bloc: BlocProvider.of<SubscribeChannelEventsBloc>(context),
-      builder: (BuildContext context, SubscribeChannelEventsState state) {
-        if (state is SubscribeChannelLoadingState) {
-          return TranslatedText('network.loading');
-        } else if (state is ChannelsUpdatedState) {
-          var length = state.channels.length;
-          var channels = state.channels;
-
-          return ListView.builder(
-            itemCount: length,
-            itemBuilder: (context, i) {
-              var channel = channels[i];
-
-              if (channel == null && i != 0) {
-                return Divider();
-              }
-
-              if (channel is EstablishedChannel) {
-                return ListTile(
-                  onTap: () => _openChannelPopup(channel),
-                  leading: _buildIcon(channel),
-                  title: _buildTitleColumn(channel),
-                  dense: true,
-                );
-              } else if (channel is PendingOpenChannel) {
-                return _buildOpenPendingListTile(channel);
-              } else if (channel is PendingClosingChannel) {
-                return _buildPendingCloseListTile(channel);
-              } else if (channel is PendingForceClosingChannel) {
-                return _buildPendingForceCloseListTile(channel);
-              } else if (channel is WaitingCloseChannel) {
-                return _buildWaitingCloseChannel(channel);
-              } else if (channel == null && i > 0) {
-                return Divider();
-              } else if (channel == null && i == 0) {
-                return Container();
-              } else {
-                return Text('Unknown Channel Type: $channel');
-              }
-            },
+    return BlocListener<CloseChannelBloc, CloseChannelBlocState>(
+      bloc: _closeChannelBloc,
+      listener: (context, state) {
+        if (state is CloseChannelErrorState &&
+            state.reason == CloseErrorReason.peerOffline) {
+          Scaffold.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                tr(
+                  context,
+                  'channels.close.partner_offline_error',
+                ),
+              ),
+            ),
           );
         }
-        return Text('Unknown State? $state');
       },
+      child: BlocBuilder(
+        bloc: BlocProvider.of<SubscribeChannelEventsBloc>(context),
+        builder: (BuildContext context, SubscribeChannelEventsState state) {
+          if (state is SubscribeChannelLoadingState) {
+            return TranslatedText('network.loading');
+          } else if (state is ChannelsUpdatedState) {
+            var length = state.channels.length;
+            var channels = state.channels;
+
+            return ListView.builder(
+              itemCount: length,
+              itemBuilder: (context, i) {
+                var channel = channels[i];
+
+                if (channel == null && i != 0) {
+                  return Divider();
+                }
+
+                if (channel is EstablishedChannel) {
+                  return ListTile(
+                    onTap: () => _openChannelPopup(channel),
+                    onLongPress: () => _closeChannelBottomSheet(channel),
+                    leading: _buildIcon(channel),
+                    title: _buildTitleColumn(channel),
+                    dense: true,
+                  );
+                } else if (channel is PendingOpenChannel) {
+                  return _buildOpenPendingListTile(channel);
+                } else if (channel is PendingClosingChannel) {
+                  return _buildPendingCloseListTile(channel);
+                } else if (channel is PendingForceClosingChannel) {
+                  return _buildPendingForceCloseListTile(channel);
+                } else if (channel is WaitingCloseChannel) {
+                  return _buildWaitingCloseChannel(channel);
+                } else if (channel == null && i > 0) {
+                  return Divider();
+                } else if (channel == null && i == 0) {
+                  return Container();
+                } else {
+                  return Text('Unknown Channel Type: $channel');
+                }
+              },
+            );
+          }
+          return Text('Unknown State? $state');
+        },
+      ),
     );
   }
 
@@ -95,6 +121,7 @@ class _ListChannelsPageState extends State<ListChannelsPage> {
       _sub.cancel();
       _sub = null;
     }
+    _closeChannelBloc?.close();
     super.dispose();
   }
 
@@ -109,6 +136,9 @@ class _ListChannelsPageState extends State<ListChannelsPage> {
         });
       }
     });
+
+    var subChanBloc = BlocProvider.of<SubscribeChannelEventsBloc>(context);
+    _closeChannelBloc = CloseChannelBloc(subChanBloc);
   }
 
   FlatLineChart _buildFlatLineChart(EstablishedChannel channel) {
@@ -356,5 +386,52 @@ class _ListChannelsPageState extends State<ListChannelsPage> {
         return DisplayChannelInfoPage(channel);
       }),
     );
+  }
+
+  void _closeChannelBottomSheet(Channel channel) {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) => Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          RaisedButton.icon(
+            color: sendManyOrange300,
+            label: TranslatedText('channels.close.close'),
+            icon: Icon(Icons.delete_forever),
+            onPressed: () {
+              _pushCloseChannelPage(channel);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _pushCloseChannelPage(EstablishedChannel channel) async {
+    var result = await Navigator.push<CloseChannelPageResult>(
+      context,
+      MaterialPageRoute(builder: (context) {
+        return CloseChannelPage(channel);
+      }),
+    );
+
+    if (Navigator.canPop(context)) {
+      // hide the bottom sheet
+      Navigator.pop(context);
+    }
+
+    if (result != null) {
+      _closeChannelBloc.add(
+        CloseChannelEvent(
+          channel.channelPoint,
+          force: result.force,
+          blockTarget: result.blockTarget,
+          satPerByte: result.satPerByte,
+          deliveryAddress: '',
+        ),
+      );
+    }
   }
 }
